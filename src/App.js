466915207f5379dc43a2ca4e8f6758c8e3c75e00
Raihan <rhnchdri@gmail.com>
2023-02-24 15:30:02 +0600

    router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Components/Route/Route';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="">
+      <RouterProvider router={router}>
+      </RouterProvider>
     </div>
   );
 }
