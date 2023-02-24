b21ef408af55a4029460812207e3def38abeb0d9
Raihan <rhnchdri@gmail.com>
2023-02-24 23:26:52 +0600

    banner added

--- src/Components/Pages/Banner/Banner.js
+++ src/Components/Pages/Banner/Banner.js
@@ -3,9 +3,9 @@ import img from '../../../assets/musculo.png'
 
 const Banner = () => {
     return (
-        <div style={{ background: '#7b7b7b' }}>
+        <div className='py-10' style={{ background: '#7b7b7b' }}>
             <h1 className='text-5xl font-bold uppercase text-center' style={{ color: 'rgb(204, 165, 82)' }}>Online Gym</h1>
-            <img src={img} alt="" className='ml-96' />
+            <img src={img} alt="" className='object-center ml-96' />
             <h1 className='text-4xl font-bold uppercase text-center mt-12' style={{ color: 'rgb(204, 165, 82)' }}>Train at home <br /><span className='italic lowercase'>$14.99/month</span></h1>
         </div>
     );
