15b45352472615ed35abbb2cb1e30ddcd9726963
Raihan <rhnchdri@gmail.com>
2023-02-25 08:31:40 +0600

    react icon added

--- src/Components/Pages/Fitness/FitnessCard.js
+++ src/Components/Pages/Fitness/FitnessCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BsArrowRight, IconName } from "react-icons/bs";
 
 const FitnessCard = ({ fit }) => {
     const { image, title, price, details } = fit;
@@ -6,10 +7,10 @@ const FitnessCard = ({ fit }) => {
         <div className="card w-80 glass mt-8">
             <figure><img src={image} alt="car!" className='h-56' /></figure>
             <div className="card-body">
-                <h2 className="card-title">{title}</h2>
+                <h2 className="card-title" style={{ color: 'rgb(204, 171, 82)' }}>{title}</h2>
                 <p>{details.slice(0, 100) + "..."}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Learn now!</button>
+                    <button className="btn" style={{ backgroundColor: 'rgb(204, 165, 82)', border: '2px solid rgb(204, 165, 82)' }}><BsArrowRight></BsArrowRight></button>
                 </div>
             </div>
         </div>
