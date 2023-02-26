2f8a1f41578113fd64f22c39272122a61ef71b80
Raihan <rhnchdri@gmail.com>
2023-02-26 22:42:08 +0600

    photoview added in fitnessdetails

--- src/Components/Pages/Fitness/FitnessDetails.js
+++ src/Components/Pages/Fitness/FitnessDetails.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const FitnessDetails = () => {
     const { image, title, price, details } = useLoaderData();
     return (
         <div className='m-14'>
             <div className="card w-80 glass mt-8">
-                <figure><img src={image} alt="" /></figure>
+                <figure>
+                    <PhotoProvider>
+                        <PhotoView src={image}>
+                            <img src={image} alt="" />
+                        </PhotoView>
+                    </PhotoProvider>
+                </figure>
                 <div className="card-body">
                     <h2 className="card-title" style={{ color: 'rgb(204, 171, 82)' }}>{title}</h2>
                     <p>{details}</p>
